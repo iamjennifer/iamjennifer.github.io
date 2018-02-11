6c078f084dbecc7355a7b4571ad0fd2514ea73d6
J.H <jennifer.howe@gmail.com>
2018-02-11 17:19:26 +0800

    [added] added smooth scrolling - js

--- res/js/script.js
+++ res/js/script.js
@@ -1,36 +1,51 @@
 /**
 
- CIS-132 Final
- shapeUp.js
- Purpose: The game is about matching a puzzle from a set of smaller shapes.
+ Project Name :: /i.am.jennifer/ Online Portfolio
+ Site :: iamjennifer.com
+
+ ----------------------------------------------------------
+
+ Copyright (c) 2018 jennifer howe. All rights reserved.
 
  @author Jennifer Howe
- @version 1.0 11/07/2012
- 
- */
+ @version 1.0 02/01/18
 
-/*::::::::::::::::::::: SECTION :::::::::::::::::::::*/
+ ----------------------------------------------------------
+ I have learned much from and used the plug-in where offered from:
 
-
-/**
- * -------------------------------------------------------------------
- * Function of the method
- *
- * @method ReadCookie
- * -------------------------------------------------------------------
- */
+ **/
 
 
 
+/**************************************************************/
+/* smooth scrolling                                           */
+/**************************************************************/
 
-/* INDEX PAGE START */
+$(document).ready(function(){
+    // Add smooth scrolling to all links
 
+    $("a").on('click', function(event) {
 
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
+            // Store hash
+            var hash = this.hash;
 
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
 
-
-/* INDEX PAGE END */
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
 
 
 
